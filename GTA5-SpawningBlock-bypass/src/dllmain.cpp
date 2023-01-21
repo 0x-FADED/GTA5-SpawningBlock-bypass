2d160944401cb179675c58f7871e400696c3d0ee
_Sakura <97910292+0x-FADED@users.noreply.github.com>
2023-01-21 23:19:29 +0900

    update pattern for future proofing

--- GTA5-SpawningBlock-bypass/src/dllmain.cpp
+++ GTA5-SpawningBlock-bypass/src/dllmain.cpp
@@ -2,7 +2,7 @@
 
 void init()
 {
-    hook::nop(scanner::GetAddress(L"GTA5.exe", "48 85 C0 0F 84 ? ? ? ? 8B 48 50"), 0x18); // entity request block
+    hook::nop(scanner::GetAddress(L"GTA5.exe", "48 85 C0 ? ? 38 45 ? 0F", 0x4D), 0x18); // entity request block
     hook::nop(scanner::GetAddress(L"GTA5.exe", "FF 52 ? 84 C0 ? ? 48 8B C3", 5), 2); // player spawn checks
 }
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
